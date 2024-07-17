e94d23764568c8615aee40548ffce7d60da75a14
navin-26 <05navin2000@gmail.com>
2024-07-17 20:38:53 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express(); 
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const testRoute = require('./routes/testRoute');
+
+// Use routes
+app.use('/api', testRoute);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
