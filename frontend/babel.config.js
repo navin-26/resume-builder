4d167c5c0838650135fd3f077d7190b05735d835
navin-26 <05navin2000@gmail.com>
2024-08-02 01:12:17 +0530

    modified

---
+++ frontend/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      'react-app'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator'
+    ]
+  };
+  
