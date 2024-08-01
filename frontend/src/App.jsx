2dbe1ac321946b68cb9ab7c98fe9266003da9c4a
navin-26 <05navin2000@gmail.com>
2024-08-01 23:27:37 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import HomePage from './pages/HomePage';
 import UserHomePage from './pages/UserHomePage';
 import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
 import TemplateEditPage from './pages/TemplateEditPage';
 import Templates from './pages/TemplatePage';
-import DashBoard from './pages/DashBoard';
 import PreviewPage from './pages/PreviewPage';
 import { AuthProvider } from './components/context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
@@ -24,7 +22,6 @@ const AppRoutes = () => {
 
         <Route element={<PrivateRoute />}>
           <Route path="/TemplateEditPage/:templateId" element={<TemplateEditPage />} />
-          <Route path="/Dashboard" element={<DashBoard />} />
           <Route path="/PreviewPage" element={<PreviewPage />} />
         </Route>
 
